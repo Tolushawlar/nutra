78ac4b4a63593274ba954bbd44287420063ceb8c
Ajemark <ajemark0110@gmail.com>
2024-05-22 13:58:39 +0100

    Payment flow for instant and schedule orders working 03

--- app/Assets/index.ts
+++ app/Assets/index.ts
@@ -1,10 +1,10 @@
 import yam from "./yam.jpg";
 import sub1 from "./sub1.png";
-// import consult from "./consultation.jpg";
+import consult from "./consultation.jpg";
 import jrice from "./jrice1.jpg";
 import jricewfish from "./jrice w fish.jpg";
 import bgImage from "./bg-image.jpg";
 import deleteIcon from "./homepage/delete.svg";
 import mealBg from "./IMG(9).png";
 
-export { yam, jrice, jricewfish, bgImage, sub1, mealBg, deleteIcon };
+export { yam, jrice, jricewfish, bgImage, consult, sub1, mealBg, deleteIcon };
